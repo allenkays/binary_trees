7a6495a5bcfb1ec5d2eb325ce7a1e76e74c5ace4
allenkays <allenkays@tutanota.com>
2023-03-01 12:20:50 -0500

    Move check condition to top

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,13 +11,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *newNode;
 
 	newNode = malloc(sizeof(binary_tree_t));
+	if (!newNode)
+		return (NULL);
 
 	newNode->n = value;
 	newNode->parent = parent;
 	newNode->left = NULL;
 	newNode->right = NULL;
 
-	if (!newNode)
-		return (NULL);
 	return (newNode);
 }
