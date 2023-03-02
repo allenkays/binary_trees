4f0174a325a0f779577f6d2499c84796868cc7ad
allenkays <allenkays@tutanota.com>
2023-03-02 03:56:16 -0500

    preorder traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,13 @@
+/**
+ * binary_tree_preorder - goes through a tree in preorder mode
+ * @tree: tree to traverse
+ * @func: pointer to function to call on each node
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
