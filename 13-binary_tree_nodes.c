57e3f3c1fb29ec0f0e4e0e299c988e01f388eed9
allenkays <allenkays@tutanota.com>
2023-03-02 13:37:06 -0500

    Edit function

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,4 +1,4 @@
-#install "binary_trees.h"
+#include "binary_trees.h"
 
 /**
  * binary_tree_nodes - counts the nodes with atleast one child
@@ -7,7 +7,13 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (!tree || (!tree->left && !tree->right))
-		return (0);
-	return (binary_tree_size(tree) - binary_tree_leaves(tree));
+	size_t nodes = 0;
+
+	if (tree)
+	{
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
+	return (nodes);
 }
